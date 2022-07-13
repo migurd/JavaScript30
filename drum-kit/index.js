4af6a1e3f75d0d100f31d133de1712c873cd25b2
migurd <angelquimorita@gmail.com>
2022-07-12 23:18:52 -0600

    1. Audios added and interface done, just need to detect the key to make it play the audio. Also, I want to add some kind of subtitles in japanese when you press a key, but I think it'll a lot of job, sooooooooooooooo

---
+++ drum-kit/index.js
@@ -0,0 +1,17 @@
+
+
+
+
+
+
+
+
+const rudeus = new Audio("./src/rudeus.mp3");
+const roxy = new Audio("./src/roxy.mp3");
+const sylphiette = new Audio("./src/sylphiette.mp3");
+const eris = new Audio("./src/eris.mp3");
+const orsted = new Audio("./src/orsted.mp3");
+const kishirika = new Audio("./src/kishirika.mp3");
+const ruijerd = new Audio("./src/ruijerd.mp3");
+const norn = new Audio("./src/norn.mp3");
+const aisha = new Audio("./src/aisha.mp3");
