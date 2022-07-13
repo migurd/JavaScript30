5038bd1030380c385de786e8e3f406eb147dc446
migurd <angelquimorita@gmail.com>
2022-07-13 14:51:38 -0600

    First commit second method

--- drum-kit/index.js
+++ drum-kit/index.js
@@ -8,38 +8,3 @@ const ruijerd = new Audio("./src/ruijerd.mp3");
 const norn = new Audio("./src/norn.mp3");
 const aisha = new Audio("./src/aisha.mp3");
 
-const box = document.querySelector('.square');
-
-const pressedKey = e => {
-  switch(e.keyCode) {
-    case 65:
-      rudeus.play();
-      break;
-    case 83:
-      roxy.play();
-      break;
-    case 68:
-      sylphiette.play();
-      break;
-    case 70:
-      eris.play();
-      break;
-    case 71:
-      orsted.play();
-      break;
-    case 72:
-      kishirika.play();
-      break;
-    case 74:
-      ruijerd.play();
-      break;
-    case 75:
-      norn.play();
-      break;
-    case 76:
-      aisha.play();
-      break;
-  }
-}
-
-document.addEventListener("keydown", (e) => pressedKey(e));
